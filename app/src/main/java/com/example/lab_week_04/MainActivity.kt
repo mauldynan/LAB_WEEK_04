66a402cd5c5e6ea821b4946f73aee7503e0846d7
Maulidya Isnaini <163633669+mauldynan@users.noreply.github.com>
2025-09-21 18:17:12 +0700

    Commit No. 2 – Bottom Navigation

--- app/src/main/java/com/example/lab_week_04/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_04/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -24,17 +25,23 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
-        // Creating top-level destinations and adding them to the drawer
+        // Creating top level destinations and adding them to the drawer
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.listFragment,
-                R.id.favoritesFragment
+                R.id.favoritesFragment,
+                R.id.cafeFragment
             ),
             findViewById(R.id.drawer_layout)
         )
 
         setupActionBarWithNavController(navController, appBarConfiguration)
+
         findViewById<NavigationView>(R.id.nav_view)
+            ?.setupWithNavController(navController)
+
+        // Added this part only
+        findViewById<BottomNavigationView>(R.id.bottom_nav)
             ?.setupWithNavController(navController)
     }
 
